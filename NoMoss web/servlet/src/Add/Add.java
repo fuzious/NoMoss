351c348e71b901eb653f7a646649823f990e13ef
fuzious <arpitsrivastava2012@gmail.com>
2019-04-18 15:14:03 +0530

    update

--- NoMoss web/servlet/src/Add/Add.java
+++ NoMoss web/servlet/src/Add/Add.java
@@ -1,4 +1,3 @@
-package Add;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -13,43 +12,44 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/Add")
+
 public class Add extends HttpServlet {
-	
-		
-	public void service(HttpServletRequest r,HttpServletResponse s) throws IOException
-	{
-		
-		String i=r.getParameter("t1");
-		String lang=r.getParameter("t2");
-		PrintWriter out=s.getWriter();
+
+
+    public void service(HttpServletRequest r,HttpServletResponse s) throws IOException
+    {
+
+        String i=r.getParameter("t1");
+        String lang=r.getParameter("t2");
+        PrintWriter out=s.getWriter();
 //		String relativeWebPath = "/WEB-INF/static/file1.ext";
 //		String absoluteDiskPath = getServletContext().getRealPath(relativeWebPath);
 //		File file = new File(absoluteDiskPath);
 //		InputStream input = new FileInputStream(file);
-			
-		FileWriter createIn=new FileWriter(".\\input.txt");
-		BufferedWriter oin=new BufferedWriter(createIn);
-		if(lang.equals("JAVA"))
-			oin.write("//1\n");
-		if(lang.equals("C"))
-			oin.write("//2\n");
-		oin.write(i);
-		oin.close();
-		createIn.close();
-		
-		NoMoss ob=new NoMoss();
-		ob.edit();
-		ob.in.close();
-		ob.out.close();
-		
-		File finput = new File(".\\output.txt");
-		BufferedReader in = new BufferedReader(new FileReader(finput));
-		String st,pr="";
-		while((st=in.readLine())!=null)
-		{
-			pr+=(st+"\n");
-		}
-		out.write(pr);;
-		out.close();
-	}
+
+        FileWriter createIn=new FileWriter(".\\input.txt");
+        BufferedWriter oin=new BufferedWriter(createIn);
+        if(lang.equals("JAVA"))
+            oin.write("//1\n");
+        if(lang.equals("C"))
+            oin.write("//2\n");
+        oin.write(i);
+        oin.close();
+        createIn.close();
+
+        NoMoss ob=new NoMoss();
+        ob.edit();
+        ob.in.close();
+        ob.out.close();
+
+        File finput = new File(".\\output.txt");
+        BufferedReader in = new BufferedReader(new FileReader(finput));
+        String st,pr="";
+        while((st=in.readLine())!=null)
+        {
+            pr+=(st+"\n");
+        }
+        out.write(pr);;
+        out.close();
+    }
 }
